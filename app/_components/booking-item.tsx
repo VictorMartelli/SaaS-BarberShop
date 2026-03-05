b1718c4af3b28d53b9f0acabc0b4a5b05473b842
VictorMartelli <victormartelligp@gmail.com>
2026-03-05 14:22:46 -0300

    feat: add bookings page

--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -1,12 +1,14 @@
-import { Avatar, AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
 import { Card } from "./ui/card";
+import { Avatar } from "./ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
 
 interface BookingItemProps {
   serviceName: string;
   barbershopName: string;
   barbershopImageUrl: string;
   date: Date;
+  status: "confirmed" | "finished";
 }
 
 const BookingItem = ({
@@ -14,38 +16,47 @@ const BookingItem = ({
   barbershopName,
   barbershopImageUrl,
   date,
+  status,
 }: BookingItemProps) => {
   return (
-    <div>
-      <Card className="flex w-full min-w-full flex-row items-center justify-between p-0">
-        <div className="flex flex-1 flex-col gap-4 p-4">
-          <Badge>Confirmado</Badge>
+    <Card className="flex h-full w-full min-w-full flex-row items-center justify-between p-0">
+      <div className="flex flex-1 flex-col gap-4 p-4">
+        <Badge
+          className={
+            status === "confirmed"
+              ? "bg-primary/10 text-primary uppercase"
+              : "bg-muted text-muted-foreground uppercase"
+          }
+        >
+          {status === "confirmed" ? "Confirmado" : "Finalizado"}
+        </Badge>
 
-          <div className="flex flex-col gap-2">
-            <p className="font-bold">{serviceName}</p>
-            <div className="flex items-center gap-2">
-              <Avatar className="h-6 w-6">
-                <AvatarImage src={barbershopImageUrl} />
-              </Avatar>
-              <p className="text-muted-foreground text-sm">{barbershopName}</p>
-            </div>
+        <div className="flex flex-col gap-2">
+          <p className="font-bold">{serviceName}</p>
+          <div className="flex items-center gap-2">
+            <Avatar className="h-6 w-6">
+              <AvatarImage src={barbershopImageUrl} />
+            </Avatar>
+            <p className="text-sm">{barbershopName}</p>
           </div>
         </div>
+      </div>
 
-        <div className="flex h-full flex-col items-center justify-center border-l p-4 py-3">
-          <p className="text-xs capitalize">
-            {date.toLocaleDateString("pt-BR", { month: "long" })}
-          </p>
-          <p>{date.toLocaleDateString("pt-BR", { day: "2-digit" })}</p>
-          <p className="text-xs capitalize">
-            {date.toLocaleTimeString("pt-BR", {
-              hour: "2-digit",
-              minute: "2-digit",
-            })}
-          </p>
-        </div>
-      </Card>
-    </div>
+      <div className="flex h-full w-[106px] flex-col items-center justify-center border-l py-3">
+        <p className="text-xs capitalize">
+          {date.toLocaleDateString("pt-BR", { month: "long" })}
+        </p>
+        <p className="text-2xl">
+          {date.toLocaleDateString("pt-BR", { day: "2-digit" })}
+        </p>
+        <p className="text-xs">
+          {date.toLocaleTimeString("pt-BR", {
+            hour: "2-digit",
+            minute: "2-digit",
+          })}
+        </p>
+      </div>
+    </Card>
   );
 };
 
